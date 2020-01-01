898e5d0243f9843649673e142a34369be0721ba3
M-Goldman <macalleegoldman7@gmail.com>
2020-01-01 14:44:04 -0500

    Updated README, excluded unnessary files and folders, added project images.

--- README.md
+++ README.md
@@ -1,3 +1,35 @@
-# windows10Calculator
-# Windows10-Calculator
-# Windows10-Calculator
+# Windows 10 Calculator
+
+A knock-off of the Windows 10 Calculator made with HTML, CSS, vanilla JS.
+
+## General Info
+
+Inspired by the Odin project. Created by myself to practice and demonstrate some of my current JS, CSS, and general Web Developer abilities.
+
+Motivation for using ES6 classes for this project came from [WebDevSimplified](https://github.com/WebDevSimplified/Vanilla-JavaScript-Calculator). The purpose of this project was to improve my programmer problem solving skills and to reinforce my coding style to use best practices.
+
+For this project, I copied [WebDevSimplifed's initial calculator](https://github.com/WebDevSimplified/Vanilla-JavaScript-Calculator)by hand-copying only when absolutely necessary. For the rest, I would seek to figure out how to implement his class methods or CSS styles myself. Afterwards, I then added additional calculator features, changed the style to match the Windows 10 Calculator, and simplified WebDev's calculator computational logic.
+
+The point of this project was never to reinvent the wheel, rather to strengthen my coding skills and to promote coding with best practices.
+
+## Screenshot
+
+My Window's 10 Calculator
+![Example screenshot](https://github.com/MNGoldman/Windows10-Calculator/images/MyWindows10Calculator.JPG)
+
+The Window's 10 Calculator
+![Example screenshot](https://github.com/MNGoldman/Windows10-Calculator/images/RealWindows10Calculator.JPG)
+
+## Technologies
+
+* HTML
+* CSS
+* Javascript
+
+## Setup
+
+The live version of the website can be seen [here](https://mngoldman.github.io/Windows10-Calculator/).
+
+## Status
+
+The project is finished for now.
---
+++ src/app.js
@@ -0,0 +1,146 @@
+  // 💯 ✌
+class Calculator {
+  constructor(previousOperandTextElement, currentOperandTextElement) {
+    this.previousOperandTextElement = previousOperandTextElement
+    this.currentOperandTextELement = currentOperandTextElement
+    this.clear();
+  }
+
+  delete() {
+    // Slice off "delete" last most digit
+    this.currentOperand = this.currentOperand.toString().slice(0, -1)
+  }
+
+  clear() {
+    // Clear all operands and operator bindings
+    this.previousOperand = ''
+    this.currentOperand = ''
+    this.operator = undefined
+  }
+
+  appendNumber(number) {
+    // Add Number to the end of display and stringify, also validates to not allow multiple decimal points
+    if (number === '.' && this.currentOperand.includes('.')) return
+    this.currentOperand = this.currentOperand + number.toString()
+  }
+
+  formatNumber(number) {
+    // Format number input by converting to string and spliting by deciaml points, then use toLocaleString
+    number = Math.round(number * 100 + Number.EPSILON) / 100
+    const stringNum = number.toString()
+    const integerNum = parseFloat(stringNum.split('.')[0])
+    let decimalDigits = stringNum.split('.')[1]
+    let integerDisplay
+
+    isNaN(integerNum) ? integerDisplay = "." : integerDisplay = integerNum.toLocaleString('en', {
+      maximumFractionDigits: 0
+    })
+
+    if (decimalDigits != null) {
+      return `${integerDisplay}.${decimalDigits}`
+    } else {
+      return integerDisplay
+    }
+  }
+
+  updateDisplay() {
+    // Update current operand
+    this.currentOperandTextELement.innerText = this.formatNumber(this.currentOperand)
+    // Update previous operand
+    if (this.operator != null) {
+      this.previousOperandTextElement.innerText = `${this.formatNumber(this.previousOperand)} ${this.operator}`
+    } else {
+      this.previousOperandTextElement.innerText = ''
+    }
+  }
+
+  chooseOperator(operator) {
+    /* Performance the calculator feature where imputs will perform selected 
+    operation in-between steps for multiple operations */
+    if (this.currentOperand === '') return
+    if (this.previousOperand !== '.') {
+      this.compute()
+    }
+
+    if (OneInputOperators.includes(operator)) {
+      this.operator = operator
+      this.compute()
+    } else {
+      this.operator = operator
+      this.previousOperand = this.currentOperand
+      this.currentOperand = ''
+    }
+  }
+
+  compute() {
+    // Perform Math, convert strings back to numbers
+    let computation
+    let curr = parseFloat(this.currentOperand)
+    let prev = parseFloat(this.previousOperand)
+    if (isNaN(curr)) return
+
+    switch (this.operator) { case `${this.operator}`:
+      computation = mathFunctions[this.operator](prev, curr); break; default: return; }
+
+    // Update variables
+    this.currentOperand = computation
+    this.previousOperand = ''
+    this.operator = undefined
+  }
+}
+
+// Initalize DOM Bindings
+const numberButtons = document.querySelectorAll('[data-number]'),
+  operationButtons = document.querySelectorAll('[data-operation]'),
+  deleteButton = document.querySelector('[data-delete]'),
+  allClearButton = document.querySelector('[data-all-clear]'),
+  equalsButton = document.querySelector('[data-equals]'),
+  previousOperandTextElement = document.querySelector('[data-previous-operand]'),
+  currentOperandTextELement = document.querySelector('[data-current-operand]');
+
+// Initalize Calculator
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextELement)
+
+// Initalize Data Structures
+const OneInputOperators = ['±', '𝓍²', '¹/𝓍', '√'],
+
+mathFunctions = {
+  '±'(prev, curr) {return curr * -1},
+  '¹/𝓍'(prev, curr) {return 1 / curr},
+  '𝓍²'(prev, curr) {return curr ** 2},
+  '√'(prev, curr) {return Math.sqrt(curr)},
+  '÷'(prev, curr) {return curr === 0 ? 0 : prev / curr},
+  '−'(prev, curr) {return prev - curr},
+  '+'(prev, curr) {return prev + curr},
+  '×'(prev, curr) {return prev * curr}
+};
+
+// Initalize Event Listeners
+numberButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    calculator.appendNumber(button.innerText)
+    calculator.updateDisplay()
+  })
+})
+
+operationButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    calculator.chooseOperator(button.innerText)
+    calculator.updateDisplay()
+  })
+})
+
+deleteButton.addEventListener('click', () => {
+  calculator.delete()
+  calculator.updateDisplay()
+})
+
+allClearButton.addEventListener('click', () => {
+  calculator.clear()
+  calculator.updateDisplay()
+})
+
+equalsButton.addEventListener('click', () => {
+  calculator.compute()
+  calculator.updateDisplay()
+})
